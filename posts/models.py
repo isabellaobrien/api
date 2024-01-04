f3ce23b73f5cd122c87faa2b8324b28310a98f4f
isabellaobrien <bellaobrien.it@gmail.com>
2024-01-04 16:56:58 +0000

    Create posts app & Post Model

---
+++ posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    owner = models.ForeignKeyField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to='images/', default='../tsqbsbfnbkcltwv6y9vt'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
