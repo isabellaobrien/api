ea52bc798162853f7a88a98e2e0cfd5e45ae5dc4
isabellaobrien <bellaobrien.it@gmail.com>
2024-01-05 10:36:45 +0000

    Add CommentList and CommentDetail views

--- comments/models.py
+++ comments/models.py
@@ -6,7 +6,7 @@ class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
     class Meta:
--- comments/serializers.py
+++ comments/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from .models import Comment
 
-class CommentSerializers(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Comment
