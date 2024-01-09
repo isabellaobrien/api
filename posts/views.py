280c09d974a5d4e84c98698b66ffa40da062683b
isabellaobrien <bellaobrien.it@gmail.com>
2024-01-09 13:21:06 +0000

    Add fields and filters to Posts

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,25 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from .models import Post
 from .serializers import PostSerializer
 from social.permissions import IsOwnerOrReadOnly 
+from django.db.models import Count
 
 class PostList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('like', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -14,4 +27,7 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PostSerializer
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('like', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
