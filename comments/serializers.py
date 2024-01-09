0b3a4279d71abc2e81d92b33f5de1f9fd2d8c069
isabellaobrien <bellaobrien.it@gmail.com>
2024-01-09 14:53:34 +0000

    Add fields and filters to Comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,20 +1,36 @@
 from rest_framework import serializers
 from .models import Comment
+from comment_likes.models import CommentLike
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    comment_like_id = serializers.SerializerMethodField()
+    comment_likes_count = serializers.ReadOnlyField()
+    comment_reply_count = serializers.ReadOnlyField()
+
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_comment_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            comment_like = CommentLike.objects.filter(
+                 owner=user, comment=obj
+            ).first()
+            return comment_like.id if comment_like else None
+        return None
 
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'post', 'created_at', 'updated_at', 'content', 'is_owner', 'profile_id', 'profile_image'
+            'id', 'owner', 'post', 'created_at', 'updated_at', 'content',
+            'is_owner', 'profile_id', 'profile_image', 'comment_like_id', 
+            'comment_likes_count','comment_reply_count'
         ]
 
 class CommentDetailSerializer(CommentSerializer):
