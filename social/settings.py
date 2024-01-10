a004099d1c95d4a526d7bd64beb922f0b79d81b6
isabellaobrien <bellaobrien.it@gmail.com>
2024-01-10 15:11:09 +0000

    Change DEBUG

--- social/settings.py
+++ social/settings.py
@@ -63,8 +63,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
+
 ALLOWED_HOSTS = ['8000-isabellaobrien-api-l0qn15n9ykb.ws-eu107.gitpod.io', 'pp5-api-backend-822273010b59.herokuapp.com']
 
 
