bfec29487379a65fab5b8252ca75fecafe5031d5
isabellaobrien <bellaobrien.it@gmail.com>
2024-01-24 16:18:34 +0000

    Add comment reply filter

--- comment_reply/views.py
+++ comment_reply/views.py
@@ -1,7 +1,8 @@
 from .models import CommentReply
 from .serializers import CommentReplySerializer, CommentReplyDetailSerializer
-from rest_framework import generics, permissions 
+from rest_framework import generics, permissions, filters
 from social.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CommentReplyList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -11,7 +12,17 @@ class CommentReplyList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'comment',
+    ]
+
 class CommentReplyDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentReplyDetailSerializer
     queryset = CommentReply.objects.all()
+
+    
