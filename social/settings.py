7b024e24c3d5976d7f4fc0192366616a1bb1a8b7
isabellaobrien <bellaobrien.it@gmail.com>
2024-02-22 14:25:24 +0000

    Add CORS_ALLOWED_ORIGINS

--- social/settings.py
+++ social/settings.py
@@ -119,6 +119,10 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
+CORS_ALLOWED_ORIGINS = [
+os.environ.get('CLIENT_ORIGIN')
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 
