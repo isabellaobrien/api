f748510f2c49c42d00b3dfe93f89c5b02d9d789b
isabellaobrien <bellaobrien.it@gmail.com>
2024-03-14 11:41:01 +0000

    add reply-likes-count

--- comment_reply/views.py
+++ comment_reply/views.py
@@ -2,12 +2,15 @@ from .models import CommentReply
 from .serializers import CommentReplySerializer, CommentReplyDetailSerializer
 from rest_framework import generics, permissions, filters
 from social.permissions import IsOwnerOrReadOnly
+from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 
 class CommentReplyList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = CommentReplySerializer
-    queryset = CommentReply.objects.all()
+    queryset = CommentReply.objects.annotate(
+        reply_likes_count=Count('replylike', distinct=True),
+    ).order_by('-created_at')
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
